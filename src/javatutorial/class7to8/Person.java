9fdd3c852e2c3162ca580fc186cd823e48a84434
weizhang <wei.zhang3678@gmail.com>
2020-03-11 11:15:41 -0400

    commit

--- src/javatutorial/class7to8/Person.java
+++ src/javatutorial/class7to8/Person.java
@@ -1,5 +1,63 @@
 package javatutorial.class7to8;
 
+/**
+*
+* Copyright 2020 YF Studio
+*
+* All right reserved.
+* 
+* Created on Mar 11, 2020 10:55:35 AM
+* 
+* a first class
+* 
+*/
 public class Person {
+	// fields
+	private String name;
+	private String email;
+	private String phoneNumber;
+	private int age;
+	
+	
+	public Person(String name, String email, String phoneNumber, int age) {
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.age = age;
+	}
+	
+	public void study(String course) {
+		System.out.println(name + " is studying " + course);
+	}
+	
+	public void work(String topic) {
+		System.out.println(name + " is working on " + topic);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 
 }
--- src/javatutorial/class7to8/UseObjectDemo.java
+++ src/javatutorial/class7to8/UseObjectDemo.java
@@ -1,9 +1,24 @@
 package javatutorial.class7to8;
 
+/**
+*
+* Copyright 2020 YF Studio
+*
+* All right reserved.
+* 
+* Created on Mar 11, 2020 10:55:35 AM
+* 
+* a demo to show use classes and objects
+* 
+*/
 public class UseObjectDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person san = new Person("Zhang", "zhang@gmail.com", "3065559999", 38);
+		san.work("cleaning");
+		
+		Person mary = new Person("Mary", "mary@gmail.com", "3065093678", 18);
+		mary.study("math");
 
 	}
 

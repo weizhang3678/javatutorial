6238fab60b0b2e6b99a7950165a7571d095c2f25
weizhang <wei.zhang3678@gmail.com>
2020-04-07 11:52:24 -0400

    basic demoes

--- src/javatutorial/class1to2/Mathmatics.java
+++ src/javatutorial/class1to2/Mathmatics.java
@@ -13,9 +13,50 @@ package javatutorial.class1to2;
 */
 public class Mathmatics {
 
+	
+public static int sum(int a, int b) {
+		
+		return a + b;
+		
+	}
+
+	
 	public static void main(String[] args) {
 		
+		
+		byte b1 = 127; // 0111 1111 + 1  = 1000 0000 //-0 -> -128, 0000 0000= +0
+		
+		int i1 = 10;
+		int i2 = 3;
+		System.out.println(i1/i2);// impossible to be a fraction number
+		
+			
+		System.out.println((byte)(b1 +1));
+		
+		int num2 = 10; // int is type
+		int num1 = 300;
+		num1 = num1 +300;
+		
+		
+		int s1 = sum( 300, 48475);
+		
+		
+		System.out.println(sum(100,300));
+		System.out.println(sum(400,300));
+		
+		
+		double d1 = 127;
+		
+
+		int sum = num1 + num2; // +
+		int substract = num1 - num2;// -
+		int product = num1 * num2;// *
+		int divide = num1 / num2;// /
+		int modulor = num1 % num2;
+
+		System.out.println(num1 + " and " + num2 + " sum = " + sum);
 
 	}
 
+	
 }
